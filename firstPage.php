c56a1c30a799c9da4cbe727b52a5f038f3905f4b
Soufiane KARROUMI <42331845+skarroumi@users.noreply.github.com>
2020-08-15 06:34:56 +0200

    Revert "Commit D'ajout"
    This reverts commit 140dd810528eca26b5db7aaedf2197617027dfcc.

--- firstPage.php
+++ firstPage.php
@@ -1,115 +1,26 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="utf-8" />
+<head>
+        <meta charset="utf-8" />
         <link rel="stylesheet" href="style.css" />
-		<script src="JavaS.js"></script>
-		<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-		<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
         <title>Pas de titre</title>
     </head>
-	
-	<body>
-
-
-<!-- Modal -->
-<div class="modal fade" id="addDataModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        
-			<select name="donneeAajouter" id="donneeAajouter">
-				<option value="Livre">Livre</option>
-				<option value="Auteur">Auteur</option>
-				<option value="Libraire">Libraire</option>
-				<option value="Edition">Edition</option>
-				<option value="Commande">Commande</option>
-			</select>
-			<br><br>		
-      </div>
-	  
-	      <div class="Livre" id="Livre">
-			<label for="ISBN">ISBN<span>*</span></label>
-			<input type="text">
-			<label for="Titre">Titre <span>*</span></label>
-			<input type="text">
-			<label for="Prize">Prix<span>*</span></label>
-			<input type="text">
-    </div>
-
-
-    <div class="Auteur" id="Auteur">
-        <label for="Pseudonyme">Pseudonyme<span>*</span></label>
-			<input type="text">
-			<label for="Nom">Nom <span>*</span></label>
-			<input type="text">
-			<label for="Prenom">Prenom<span>*</span></label>
-			<input type="text">         
-    </div>
-
-	
-    <div class="Libraire" id="Libraire">
-        <label for="CdeLibraire">Code Libraire<span>*</span></label>
-			<input type="text">
-			<label for="Nom">Nom <span>*</span></label>
-			<input type="text">
-			<label for="CdeAdresse">Code Adresse<span>*</span></label>
-			<input type="text">
-    </div>
-	
-	
-	<div class="Edition" id="Edition">
-			<label for="NumEdition">Numero d'edition<span>*</span></label>
-			<input type="text">
-			<label for="Annee">Annee <span>*</span></label>
-			<input type="text">
-			<label for="NombreExemplaire">Nombre d'exemplaire<span>*</span></label>
-			<input type="text">
-			<label for="Prix">Prix<span>*</span></label>
-			<input type="text">
-			<label for="ISBN">ISBN <span>*</span></label>
-			<input type="text">
-					
-    </div>
-	
-    <br><br>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
-
-		<div class="container">
-			<div class="jumbotron">
-				<!-- Button trigger modal -->
-				<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addDataModal">
-					Ajouter
-				</button>	
-			</div>
-		</div>
-
-		<form action="#" method="POST">
-			<select name="valeurChoisie" id="valeurChoisie">
-				<option value="Livre">Livre</option>
-				<option value="Auteur">Auteur</option>
-				<option value="Libraire">Libraire</option>
-				<option value="Edition">Edition</option>
-				<option value="Commande">Commande</option>
-			</select>
-			<br><br>
-			<input type="submit" value="Chercher">
-		</form> 
+<body>
+<form action="#" method="POST">
+  <select name="valeurChoisie" id="valeurChoisie">
+    <option value="Livre">Livre</option>
+    <option value="Auteur">Auteur</option>
+    <option value="Libraire">Libraire</option>
+    <option value="Edition">Edition</option>
+    <option value="Commande">Commande</option>
+  </select>
+  <br><br>
+  <input type="submit" value="Chercher">
+</form> 
 <?php include'connexion.php'; ?>
 <?php 
     if(isset($_POST['valeurChoisie']))
@@ -144,9 +55,9 @@
             switch($valeurChoisie)
             { 
                 case 'Livre' :  
-				
+                 echo $data['Titre'];
                 echo'<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#id'.$data['ISBN'].'">
-                 '.$data['Titre'].'
+                 plus de infos
                 </button>
                
                <div class="modal fade" id="id'.$data['ISBN'].'" tabindex="-1" role="dialog" aria-labelledby="id'.$data['ISBN'].'" aria-hidden="true">
@@ -158,17 +69,16 @@
                          <span aria-hidden="true">Livre</span>
                        </button>
                      </div>
-					 
                      <div class="modal-body">
-                      '.$data['ISBN'].'
-					  '.$data['Titre'].'
-					  '.$data['Prize'].'                 
+                      '.$data['Titre'].'
+                      
+                     
+                     
                      </div>
-					 
                      <div class="modal-footer">
-                       <button type="button" class="btn btn-primary" data-dismiss="modal">Retourner</button>                   
+                       <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+                       
                      </div>
-					 
                    </div>
                  </div>
                </div>'; 
